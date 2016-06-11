23c398ff622cc17be23da7db06cb030aaa1a2576
Ard0ise <ardoise.gisement@gmail.com>
2016-06-11 08:02:03 +0200

    init logstash csv2json

--- process/csv2json.sh
+++ process/csv2json.sh
@@ -6,7 +6,7 @@
 
 SCRIPT_DIR=$(dirname "$0")
 SCRIPT_NAME=$(basename "$0")
-NIFI_HOME=$(cd "${SCRIPT_DIR}" && cd .. && pwd)
+KATASTEL_HOME=$(cd "${SCRIPT_DIR}" && cd .. && pwd)
 PROGNAME=$(basename "$0")
 
 warn() { echo "${PROGNAME}: $*"; }
@@ -16,6 +16,7 @@ init() {
   ##;
 }
 run() {
+  LOGSTASH_CONF_DIR="${KATASTEL_HOME}/conf"
   ##;
 }
 main() {
